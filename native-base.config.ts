c5aea221eddf1f302178a396749c009cd33c5409
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-08-06 09:52:35 -0300

    feat: config theme

---
+++ native-base.config.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from "native-base";
+
+export const theme = extendTheme({
+  colors: {
+    primary: {
+      400: '#313144',
+      500: '#1f1f2b'
+    },
+    secondary: {
+      500: '#fed95f'
+    }
+  },
+  fontConfig: {
+
+  },
+  fonts: {
+
+  },
+  config: {
+    initialColorMode: 'dark'
+  }
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Box, NativeBaseProvider, Text } from 'native-base';
+import { theme } from '../native-base.config';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Box safeArea flex={1} backgroundColor="#121212">
-        <Text color="#ffffff">Hello app!</Text>
+    <NativeBaseProvider theme={theme}>
+      <Box safeArea flex={1} backgroundColor="primary.500">
+        <Text color="secondary.500">Hello app!</Text>
       </Box>
     </NativeBaseProvider>
   );
